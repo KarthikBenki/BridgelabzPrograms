aed5ee13559d940a08bcc9233009f01966578af2
karthikbenki <karthikmc007@gmail.com>
2022-01-26 07:40:26 +0530

    array programs

--- day7doc1/range_repeated.sh
+++ day7doc1/range_repeated.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -a arr
+index=0
 for ((i=1;i<=100;i++))
 do
 	for ((j=i,rev=0;j!=0;j=j/10))
@@ -11,8 +13,10 @@ do
 			if ((rev > 10))
 			then
 				printf "$rev "
+				arr[((index++))]=$rev
 			fi
 		fi
 	done
 done
 printf "\n"
+echo ${arr[@]}
